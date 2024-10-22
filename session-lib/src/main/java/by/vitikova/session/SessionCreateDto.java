23f856d6a660df662bd2a019bd97369b17e0fdb2
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-10-22 09:59:02 +0300

    clean SessionCreateDto

--- session-lib/src/main/java/by/vitikova/session/SessionCreateDto.java
+++ session-lib/src/main/java/by/vitikova/session/SessionCreateDto.java
@@ -10,31 +10,8 @@ import lombok.ToString;
 public final class SessionCreateDto implements SessionInfo {
     private final String login;
 
-    //    public SessionCreateDto(String login) {
-//        this.login = login;
-//    }
-//
     @Override
     public String login() {
         return login;
     }
-
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (obj == this) return true;
-//        if (obj == null || obj.getClass() != this.getClass()) return false;
-//        var that = (SessionCreateDto) obj;
-//        return Objects.equals(this.login, that.login);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(login);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "SessionCreateDto[" +
-//                "login=" + login + ']';
-//    }
 }
